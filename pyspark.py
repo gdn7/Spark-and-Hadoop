5e29d776590977af4c8dd6ad83295ca4bfd25756
Niranjan GD <niranj@okstate.edu>
2017-08-08 21:04:42 -0700

    Update pyspark.py

--- pyspark.py
+++ pyspark.py
@@ -380,15 +380,10 @@ order by q1.order_date;
 # Filter data into a smaller dataset using Spark
 ordersRDD = sc.textFile("/user/cloudera/sqoop_import/orders")
 for i in ordersRDD.filter(lambda line: line.split(",")[3] == "COMPLETE").take(5): print(i)
-
 for i in ordersRDD.filter(lambda line: "PENDING" in line.split(",")[3]).take(5): print(i)
-
 for i in ordersRDD.filter(lambda line: int(line.split(",")[0]) > 100).take(5): print(i)
- 
 for i in ordersRDD.filter(lambda line: int(line.split(",")[0]) > 100 or line.split(",")[3] in "PENDING").take(5): print(i)
- 
 for i in ordersRDD.filter(lambda line: int(line.split(",")[0]) > 1000 and ("PENDING" in line.split(",")[3] or line.split(",")[3] == ("CANCELLED"))).take(5): print(i)
- 
 for i in ordersRDD.filter(lambda line: int(line.split(",")[0]) > 1000 and line.split(",")[3] != ("COMPLETE")).take(5): print(i)
 
 #Check if there are any cancelled orders with amount greater than 1000$
